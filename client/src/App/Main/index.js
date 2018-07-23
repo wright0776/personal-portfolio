a884e0b02f624123584acd2eb590b29aae125227
wright0776 <matwright2010@gmail.com>
2018-07-23 08:36:42 -0600

    changes made to main page card

--- client/src/App/Main/index.js
+++ client/src/App/Main/index.js
@@ -6,23 +6,26 @@ class Main extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            spin: 270,
+            spinY: '270deg',
+            spinX: '0deg',
             animation: 'spinin 2s forwards',
-            front: true
         }
     }
 
     spinAgain = () => {
-        this.setState({spin: `${Math.floor(Math.random() * 1080)}deg`,animation: 'none'});
-        console.log(this.state.spin)
+        this.setState({ spinY: `${Math.floor(Math.random() * 1080)}deg`, animation: 'none' });
     }
     flipOver = () => {
-        this.setState(prevState => ({front: !prevState.front}))
+        if(this.state.spinX === '0deg'){
+            this.setState({ spinX: '180deg' });
+        } else {
+            this.setState({ spinX: '0deg' })
+        }
     }
-    componentDidMount () {
+    componentDidMount() {
         setTimeout(() => {
             this.setState({
-                spin: '-5deg',
+                spinY: '-5deg',
                 animation: 'none'
             })
         }, 2000)
@@ -31,12 +34,20 @@ class Main extends Component {
     render() {
         return (
             <div className="main">
-                <div className='title-container' 
-                    onClick={this.spinAgain} 
-                    style={{transform: `rotateY(${this.state.spin})`, animation: this.state.animation}} 
-                    >
-                    <h1 className='title'>Matt</h1>
-                    <p className='subtitle'>Certified Full Stack Web Developer</p>
+                <div className='card-container'
+                    onClick={this.flipOver}
+                    style={{ transform: `rotateY(${this.state.spinY}) rotateX(${this.state.spinX})`, animation: this.state.animation }}
+                >
+                    <div className='card-front'>
+                        <h1 className='title'>Matt Wright</h1>
+                        <p className='subtitle'>Certified Full Stack Web Developer</p>
+                    </div>
+                    <div className='card-back'>
+                        <span className='card-info1'>Matthew Wright</span>
+                        <span className='card-info2'>matthewwright.io</span>
+                        <span>matwright2010@gmail.com</span>
+                        <span>801-755-0125</span>
+                    </div>
                 </div>
                 <div className='main-links'>
                     <Link className='black' to='/portfolio'>
